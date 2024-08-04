90add0c7cee0fbc03849b2dca4474a8f90a70509
Chloeddd <2735466524@qq.com>
2024-08-04 21:12:41 +0800

    lab Alarm

--- kernel/trap.c
+++ kernel/trap.c
@@ -83,7 +83,7 @@ usertrap(void)
       p->ticks_num++;
       if(p->ticks_num > p->interval){
         p->ticks_num=0;
-        p->tp = *p->trapframe; //store trapframe
+        p->pre_p = *p->trapframe; //store trapframe
         p->trapframe->epc=p->handler; //epc用于保存发生异常时的程序计数器值
 
       }
