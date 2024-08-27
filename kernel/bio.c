9b3cf889988c0c756fbbd62d75fa2e548ca37ac3
Chloeddd <2735466524@qq.com>
2024-08-27 18:34:03 +0800

    lab Buffer Cache

--- kernel/bio.c
+++ kernel/bio.c
@@ -23,9 +23,12 @@
 #include "fs.h"
 #include "buf.h"
 
+#define BSIZE 13
+
 struct {
-  struct spinlock lock;
   struct buf buf[NBUF];
+  struct buf bucket[BSIZE];
+  struct spinlock lock[BSIZE];
 
   // Linked list of all buffers, through prev/next.
   // Sorted by how recently the buffer was used.
@@ -38,18 +41,26 @@ binit(void)
 {
   struct buf *b;
 
-  initlock(&bcache.lock, "bcache");
+  int 
 
-  // Create linked list of buffers
-  bcache.head.prev = &bcache.head;
-  bcache.head.next = &bcache.head;
-  for(b = bcache.buf; b < bcache.buf+NBUF; b++){
-    b->next = bcache.head.next;
-    b->prev = &bcache.head;
-    initsleeplock(&b->lock, "buffer");
-    bcache.head.next->prev = b;
-    bcache.head.next = b;
+  for(int i=0;i<BSIZE;i++){
+    initlock(&bcache.lock[1], "bcache");
+    //初始化双向链表
+    bcache.bucket[i].prev = &bcache.bucket[i];
+    bcache.bucket[i].next = &bcache.bucket[i];
   }
+
+  for(b = bcache.buf; b < bcache.buf+NBUF; b++){
+    int bucketNo = b%BSIZE;
+    //根据哈希桶，将buf插入双向链表
+    b->next = bcache.bucket[bucketNo].next;
+    b->prev = &bcache.bucket[bucketNo];
+    initsleeplock(&b->lock, "buffer");
+    bcache.bucket[bucketNo].next->prev = b;
+    bcache.bucket[bucketNo].next = b;
+  }
+
+  
 }
 
 // Look through buffer cache for block on device dev.
@@ -59,28 +70,30 @@ static struct buf*
 bget(uint dev, uint blockno)
 {
   struct buf *b;
+  int bucketNo = blockno%BSIZE;
 
-  acquire(&bcache.lock);
+  acquire(&bcache.lock[bucketNo]);
 
   // Is the block already cached?
-  for(b = bcache.head.next; b != &bcache.head; b = b->next){
+  for(b = bcache.bucket[bucketNo].next; b != &bcache.bucket[bucketNo]; b = b->next){
     if(b->dev == dev && b->blockno == blockno){
       b->refcnt++;
-      release(&bcache.lock);
+      release(&bcache.lock[bucketNo]);
       acquiresleep(&b->lock);
       return b;
+ 
     }
   }
 
   // Not cached.
   // Recycle the least recently used (LRU) unused buffer.
-  for(b = bcache.head.prev; b != &bcache.head; b = b->prev){
+  for(b = bcache.bucket[bucketNo].prev; b != &bcache.bucket[bucketNo]; b = b->prev){
     if(b->refcnt == 0) {
       b->dev = dev;
       b->blockno = blockno;
       b->valid = 0;
       b->refcnt = 1;
-      release(&bcache.lock);
+      release(&bcache.lock[bucketNo]);
       acquiresleep(&b->lock);
       return b;
     }
@@ -120,8 +133,10 @@ brelse(struct buf *b)
     panic("brelse");
 
   releasesleep(&b->lock);
+  
+  int bucketNo = blockno%BSIZE;
 
-  acquire(&bcache.lock);
+  acquire(&bcache.lock[bucketNo]);
   b->refcnt--;
   if (b->refcnt == 0) {
     // no one is waiting for it.
